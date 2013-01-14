52bc4aa50a64c99e76929ec10e7f2d6bf42fded7
rogerjamjet <oreilp25@hotmail.com>
2013-01-14 19:42:00 +0000

    Update src/PlayWithGitHub.java

--- src/PlayWithGitHub.java
+++ src/PlayWithGitHub.java
@@ -1,8 +1,8 @@
-
 public class PlayWithGitHub
 {
 	public static void main(String... args)
 	{
 		System.out.println("playWithGitHub");
+		System.out.println("editing code with Git online... how cool is that");
 	}
 }
